af17a28292f3aea65983ac694e725091630f0210
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-08 19:43:21 -0400

    refactor: using Root class as resource for root endpoint

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 
+from Root import Root
+
 
 def create_app():
     app = Flask(__name__)
     api = Api(app)
     CORS(app)
 
-    api.add_resource(None, '/')
+    api.add_resource(Root, '/')
 
     return app

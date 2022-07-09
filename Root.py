370f3728bdfb0afc944452e642f4bb120d17ff8d
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-08 22:08:31 -0400

    feat: added expiry to cache values

--- Root.py
+++ Root.py
@@ -1,5 +1,7 @@
 import json
 
+from datetime import datetime
+
 from flask import request
 from flask_restful import Resource
 
@@ -34,7 +36,7 @@ class Root(Resource):
             print('no cache key or value received')
             return False
 
-        is_saved_successfully: bool = self.write_data(cache_key, cache_value)
+        is_saved_successfully: bool = self.write_data(cache_key, cache_value, cache_expiry)
 
         return {'is_saved_successfully': is_saved_successfully}
 
@@ -46,9 +48,25 @@ class Root(Resource):
         with open('data.json', 'r') as f:
             data: dict = json.load(f)
 
-        return data.get(key, None)
+        cache_metadata = data.get(key, None)
 
-    def write_data(self: 'Root', key: str, value: object) -> bool:
+        if not cache_metadata: return None
+
+        cache_value = cache_metadata.get('value', None)
+        cache_expiry = cache_metadata.get('expiry', None)
+
+        if not cache_value: return None
+        if not cache_expiry: return cache_value
+
+        expiry_date: datetime = datetime.fromisoformat(cache_expiry)
+        current_date: datetime = datetime.now()
+
+        cache_expired: bool = current_date > expiry_date
+        if cache_expired: return None
+
+        return cache_value
+
+    def write_data(self: 'Root', key: str, value: object, cache_expiry: int | None) -> bool:
         data: dict = request.get_json()
 
         if not data:
@@ -59,8 +77,17 @@ class Root(Resource):
             with open('data.json', 'r') as f:
                 data: dict = json.load(f)
 
+            if not cache_expiry:
+                encoded_expiry = ''
+            else:
+                expiry_date: datetime = datetime.now() + datetime.timedelta(seconds=cache_expiry)
+                encoded_expiry: str = expiry_date.isoformat()
+
             # update the data
-            data[key] = value
+            data[key] = {
+                'value': value,
+                'expiry': encoded_expiry,
+            }
 
             with open('data.json', 'w') as f:
                 json.dump(data, f)

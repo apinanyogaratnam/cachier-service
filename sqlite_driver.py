ab921094d7f8c4357be383a30a38b692c66a07fe
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-10 12:36:50 -0400

    feat: created read_data and write_data method signatures with empty bodies

--- sqlite_driver.py
+++ sqlite_driver.py
@@ -1,3 +1,9 @@
 class SqliteDriver:
     def __init__(self: 'SqliteDriver', filename: str) -> None:
         self.filename = filename
+
+    def read_data(self: 'SqliteDriver', key: str) -> object | None:
+        pass
+
+    def write_data(self: 'SqliteDriver', key: str, value: str, expiry: int | None = None) -> bool:
+        pass

053e9a0a50859b245629bad0970835fab13303a4
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-10 12:10:53 -0400

    refactor: moved script into main function to be called externally

--- scripts/create_cache_table.py
+++ scripts/create_cache_table.py
@@ -1,25 +1,31 @@
 import sqlite3
 
-connection = sqlite3.connect('cache.db')
 
-create_table_query = '''
-    CREATE TABLE IF NOT EXISTS cache (
-        cache_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-        created TIMESTAMP NOT NULL,
-        cache_key TEXT NOT NULL,
-        cache_value TEXT,
-        cache_expiry TIMESTAMP
-    );
-'''
+def main():
+    connection = sqlite3.connect('cache.db')
 
-try:
-    print('Creating table...')
-    connection.execute(create_table_query)
-    connection.commit()
-    print('Table created successfully')
-except Exception as error:
-    print(error)
-    connection.rollback()
-finally:
-    connection.close()
-    print('Connection closed')
+    create_table_query = '''
+        CREATE TABLE IF NOT EXISTS cache (
+            cache_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            created TIMESTAMP NOT NULL,
+            cache_key TEXT NOT NULL,
+            cache_value TEXT,
+            cache_expiry TIMESTAMP
+        );
+    '''
+
+    try:
+        print('Creating table...')
+        connection.execute(create_table_query)
+        connection.commit()
+        print('Table created successfully')
+    except Exception as error:
+        print(error)
+        connection.rollback()
+    finally:
+        connection.close()
+        print('Connection closed')
+
+
+if __name__ == '__main__':
+    main()

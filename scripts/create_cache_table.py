a746e3669b77e877f3157e5297eeb8bda08b2868
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-10 12:09:45 -0400

    feat: created script for creating a cache table

---
+++ scripts/create_cache_table.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+connection = sqlite3.connect('cache.db')
+
+create_table_query = '''
+    CREATE TABLE IF NOT EXISTS cache (
+        cache_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        created TIMESTAMP NOT NULL,
+        cache_key TEXT NOT NULL,
+        cache_value TEXT,
+        cache_expiry TIMESTAMP
+    );
+'''
+
+try:
+    print('Creating table...')
+    connection.execute(create_table_query)
+    connection.commit()
+    print('Table created successfully')
+except Exception as error:
+    print(error)
+    connection.rollback()
+finally:
+    connection.close()
+    print('Connection closed')

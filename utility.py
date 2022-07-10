3e4833ec032269078a92fe549a8858c8f36fd003
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-10 12:23:18 -0400

    feat: created utility functions for interacting with sqlite

---
+++ utility.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+import pandas as pd
+
+
+def get_sqlite_connection() -> sqlite3.Connection:
+    connection: sqlite3.Connection = sqlite3.connect('cache.db')
+    return connection
+
+
+def query_sqlite_database(connection: sqlite3.Connection, query: str) -> object:
+    table: pd.DataFrame = pd.read_sql_query(query, connection)
+    return table
+
+
+def write_sqlite_database(connection: sqlite3.Connection, query: str) -> bool:
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute(query)
+            connection.commit()
+            return True
+        except Exception as error:
+            print(error)
+            return False

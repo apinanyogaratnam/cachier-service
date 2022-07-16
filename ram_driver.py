cb2f46bf692d6e7cef374b8101e979e38d56926e
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-15 20:11:51 -0400

    feat: created RamDriver class with read_data method

---
+++ ram_driver.py
@@ -0,0 +1,56 @@
+from datetime import datetime, timedelta
+
+ram_storage = {}
+
+
+class RamDriver:
+    def __init__(self: 'RamDriver') -> None:
+        pass
+
+    def read_data(self: 'RamDriver', key: str) -> object | None:
+        if not key:
+            print('no key to read')
+            return None
+
+        cache_metadata = ram_storage.get(key, None)
+
+        if not cache_metadata: return None
+
+        cache_value = cache_metadata.get('value', None)
+        cache_expiry = cache_metadata.get('expiry', None)
+
+        if not cache_value: return None
+        if not cache_expiry: return cache_value
+
+        expiry_date: datetime = datetime.fromisoformat(cache_expiry)
+        current_date: datetime = datetime.utcnow()
+
+        cache_expired: bool = current_date > expiry_date
+        if cache_expired: return None
+
+        return cache_value
+
+    # def write_data(self: 'RamDriver', key: str, value: object, cache_expiry: int | None = None) -> bool:
+    #     try:
+    #         with open('data.json', 'r') as f:
+    #             data: dict = json.load(f)
+
+    #         if not cache_expiry:
+    #             encoded_expiry = ''
+    #         else:
+    #             expiry_date: datetime = datetime.now() + timedelta(seconds=cache_expiry)
+    #             encoded_expiry: str = expiry_date.isoformat()
+
+    #         # update the data
+    #         data[key] = {
+    #             'value': value,
+    #             'expiry': encoded_expiry,
+    #         }
+
+    #         with open('data.json', 'w') as f:
+    #             json.dump(data, f)
+    #     except Exception as error:
+    #         print(error)
+    #         return False
+
+    #     return True

7c6d2e40973d86891ff86017ec8a348a6f8eda35
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-17 18:40:33 -0400

    refactor: using env var to decide debug mode

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import os
 
+from dotenv import load_dotenv
+
 from app import create_app
+
+load_dotenv()
 
 
 def main():
     PORT = os.environ.get('PORT', 8000)
 
     app = create_app()
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    environment = os.environ.get('ENVIRONMENT')
+
+    debug_mode = environment.upper() == 'DEVELOPMENT'
+
+    app.run(host='0.0.0.0', port=PORT, debug=debug_mode)
 
 
 if __name__ == '__main__':

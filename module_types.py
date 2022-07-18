63a13e24e3aed3fbac4d644dd150787b5071faf6
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-17 20:11:18 -0400

    feat: created driver type

--- module_types.py
+++ module_types.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from sqlite_driver import SqliteDriver
 from json_driver import JsonDriver
 from pickle_driver import PickleDriver
@@ -6,3 +8,13 @@ from ram_driver import RamDriver
 
 class Driver(SqliteDriver, JsonDriver, PickleDriver, RamDriver):
     pass
+
+
+class DriverType(Enum):
+    SQLITE = 1
+    JSON = 2
+    PICKLE = 3
+    RAM = 4
+
+    def __eq__(self, other):
+        return self.value == other.value
